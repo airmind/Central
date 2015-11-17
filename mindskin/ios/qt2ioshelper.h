361f7afe21919a00ce9858432f24d2f61ed5d8d4
ning roland <Roland@nings-MacBook-Air.local>
2015-11-18 01:06:53 +0800

    add iOS helper

---
+++ mindskin/ios/qt2ioshelper.h
@@ -0,0 +1,18 @@
+//
+//  qt2ioshelper.h
+//  qgroundcontrol
+//
+//  Created by ning roland on 11/11/15.
+//
+//
+
+#ifndef qgroundcontrol_qt2ioshelper_h
+#define qgroundcontrol_qt2ioshelper_h
+
+class qt2ioshelper {
+
+public:
+       static NSString* QString2NSString (const QString* qstr);
+};
+
+#endif
