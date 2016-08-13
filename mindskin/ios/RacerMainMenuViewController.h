8c5d3c26206ec54d4f63bc34af7e5ba8fb30c41c
rolandash <ning.roland@gmail.com>
2016-08-13 22:04:44 +0800

    add racer main UI files

---
+++ mindskin/ios/RacerMainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  RacerMainMenuViewController.h
+//  QGroundControl
+//
+//  Created by ning roland on 8/11/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RacerMainMenuViewController : UIViewController
+
+@end
---
+++ mindskin/ios/RacerMainMenuViewController.m
@@ -0,0 +1,37 @@
+//
+//  RacerMainMenuViewController.m
+//  QGroundControl
+//
+//  Created by ning roland on 8/11/16.
+//
+//
+
+#import "RacerMainMenuViewController.h"
+
+@interface RacerMainMenuViewController ()
+
+@end
+
+@implementation RacerMainMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
