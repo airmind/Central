b06e7f34d29726878ec76d8729ad7b5ddcef436a
Caprin <478586630@qq.com>
2016-08-30 15:15:21 +0800

    modified BLEComm.java

--- android/src/org/airmind/ble/BLEComm.java
+++ android/src/org/airmind/ble/BLEComm.java
@@ -1,13 +1,38 @@
 package org.airmind.ble;
+
+import android.content.Intent;
+
 /**
-*
-* Called from Qt-C++
-*/
+ * Called from Qt-C++
+ */
 public class BLEComm {
     /**
-    * called when app starts up and auto-connect BLE device/service/characteristic which has ever been connnected before
-    */
+     * called when app starts up and auto-connect BLE device/service/characteristic which has ever been connnected before
+     */
     public static void connect(String device, String service, String characteristic) {
         System.out.println("auto connect to device:" + device + ", service:" + service + ", characteristic:" + characteristic);
+
+        // connect to device
+        connectToDeice(device);
+
+        connectSevice(service);
+
+        conncetToCharacteristic(characteristic);
+
+    }
+
+    // connect to device
+    private static void connectToDeice(String device) {
+
+    }
+
+    // conncet to service
+    private static void connectSevice(String service) {
+
+    }
+
+    // connect to characteristic
+    private static void conncetToCharacteristic(String characteristic) {
+        
     }
 }
