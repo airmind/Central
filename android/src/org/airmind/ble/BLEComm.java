61970e8d278a6c0d7b87658c9a1718fb811b51cd
Caprin <478586630@qq.com>
2016-09-07 15:40:06 +0800

    BLEComm.java

--- android/src/org/airmind/ble/BLEComm.java
+++ android/src/org/airmind/ble/BLEComm.java
@@ -1,41 +1,4 @@
 package org.airmind.ble;
-<<<<<<< HEAD
-
-import android.content.Intent;
-
-/**
- * Called from Qt-C++
- */
-public class BLEComm {
-    private static BluetoothLeService mBluetoothService;
-    /**
-     * called when app starts up and auto-connect BLE device/service/characteristic which has ever been connnected before
-     */
-    public static void connect(String device, String service, String characteristic) {
-        System.out.println("auto connect to device:" + device + ", service:" + service + ", characteristic:" + characteristic);
-
-        connectToDeice(device);
-
-        connectToSevice(service);
-
-        conncetToCharacteristic(characteristic);
-
-    }
-
-    // connect to device
-    private static void connectToDeice(String device) {
-        mBluetoothService.connect(device);
-    }
-
-    // conncet to service
-    private static void connectToSevice(String service) {
-        //TODO
-    }
-
-    // connect to characteristic
-    private static void conncetToCharacteristic(String characteristic) {
-        //TODO
-=======
 /**
 *
 * Called from Qt-C++
@@ -51,6 +14,5 @@ public class BLEComm {
     public static void write(String deviceAddress, String serviceUUID, String characteristicUUID, byte[] data) {
         System.out.println("write to deviceAddress:" + deviceAddress + ", service-UUID:" + serviceUUID + ", characteristic-uuid:" + characteristicUUID +
         ", with value:" + (data != null ? new String(data):"null"));
->>>>>>> mindskin-gumingpo
     }
 }
