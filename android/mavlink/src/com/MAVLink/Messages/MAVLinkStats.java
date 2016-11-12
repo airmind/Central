41fa2060b47dd4491effa7aaaa726960355df6cb
Mingpo GU <gumingpo@gmail.com>
2016-11-12 12:10:39 +0800

    test github

--- android/mavlink/src/com/MAVLink/Messages/MAVLinkStats.java
+++ android/mavlink/src/com/MAVLink/Messages/MAVLinkStats.java
@@ -25,6 +25,7 @@ public class MAVLinkStats /* implements Serializable */{
     /**
      * Check the new received packet to see if has lost someone between this and
      * the last packet
+     *
      * 
      * @param packet
      *            Packet that should be checked
